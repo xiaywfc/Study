20ae81ef25841652767d91c808c7c961a0c59d52
e <e>
2020-05-12 15:14:03 +0800

    还原

--- pipeline/src/pipeline-script-job.groovy
+++ pipeline/src/pipeline-script-job.groovy
@@ -2,55 +2,24 @@ import hudson.model.*;
 
 println env.JOB_NAME
 println env.BUILD_NUMBER
-
+println env.WORKSPACE
+ 
 pipeline{
 	
 	agent any
 	stages{
-		
-		stage("git checkout") {
+		stage("init") {
 			steps{
-				script {
-					checkout([$class: 'GitSCM', branches: [[name: '*/master']],
-						userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
-							url: 'https://github.com/Anthonyliu86/HelloWorld.git']]])
+				script{
+					println "欢迎来到Anthony的Pipeline教程。"
+					model_test = load env.WORKSPACE + "/pipeline/module/pipeline-demo-module.groovy"
 				}
 			}
 		}
-		stage("Get failure node") {
-			steps {
-				script {
-					
-					try{
-					    failure_node_list = []
-					    out = sh(script:" ls /tmp/test | grep .status ", returnStdout: true).toString().trim()
-					    println out
-					    lines = out.split("\n")
-					    lines.each { 
-					        if(!it.contains("STARTED")) {
-					             if(it.startsWith("ANDROID")){
-					                 failure_node_list.add("android")
-					             }else if(it.startsWith("IOS")){
-					                 failure_node_list.add("ios")
-					             }else if(it.startsWith("DB")){
-					                 failure_node_list.add("mysql")
-					             }else if(it.startsWith("TOMCAT")){
-					                 failure_node_list.add("tomcat")
-					             } else if(it.startsWith("WEB")){
-					                 failure_node_list.add("ngix")
-					             }else {
-					                 println "unknow host type."
-					             }
-
-					        }
-
-					    }
-					    println failure_node_list.toString()
-					    
-					} catch(Exception e) {
-						println e
-						error("fond error during get failure node.")
-					}
+		stage("Test Method") {
+			steps{
+				script{
+					log_files = model_test.find_files('**/*.log')
 				}
 			}
 		}
