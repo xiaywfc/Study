0671f150c45326cad71c50db6a3f7e2169a321d5
e <e>
2020-05-12 15:58:40 +0800

    huanyuan2

--- pipeline/scripts/pipeline-utility-steps-test.groovy
+++ pipeline/scripts/pipeline-utility-steps-test.groovy
@@ -2,9 +2,9 @@ import hudson.model.*;
 
 println env.JOB_NAME
 println env.BUILD_NUMBER
-
+println env.WORKSPACE
+ 
 pipeline{
-	
 	agent any
 	stages{
 		stage("init") {
@@ -14,56 +14,13 @@ pipeline{
 				}
 			}
 		}
-		stage("read properties") {
+		stage("Test Method") {
 			steps{
 				script{
-					properties_file = env.WORKSPACE + "/testdata/test.properties"
-					model_test.read_properties(properties_file)
-					println "================================"
+					log_files = model_test.find_files('**/*.log')
 				}
 			}
 		}
-		stage("touch file") {
-			steps{
-				script{
-					touch_file = env.WORKSPACE + "/testdata/"+ env.BUILD_NUMBER +".log"
-					touch touch_file
-				}
-			}
-		}
-		stage("read yaml file") {
-			steps{
-				script{
-					yaml_file = env.WORKSPACE + "/testdata/test.yml"
-					model_test.read_yaml_file(yaml_file)
-					println "=========================="
-					yaml_string = """
-                    age: 18
-                    city: 'Shanghai'
-                    isMale: false
-                    name: 'Lucy'
-                    """
-					model_test.read_yaml_file(yaml_string)
-				}
-			}
-		}
-		stage("write into yaml file") {
-			steps{
-				script{
-					def amap = [name: 'Anthony',
-								age : 18,
-								city: 'Beijing',
-								isMale: true
-								]
-					yaml_file = env.WORKSPACE + "/testdata/new.yml"
-					model_test.write_to_yaml(amap, yaml_file)
-					println "the contents of yaml file are: "
-					model_test.read_yaml_file(yaml_file)
-				}
-			}
-		}
-		
 	}
 }
-
 
